8991580b8301c648358ed5fc11f9b5a0f1e2ccad
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-08-06 23:29:01 +0300

    set r3f-content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -69,8 +69,8 @@ const Hero = () => {
             <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">JohnKennedy</span></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}> I develop 3D visuals, user <br className="sm:block hidden"/>interfaces and web applications.</p>
           </div>
-          <ComputersCanvas/>
       </div>
+          <ComputersCanvas/>
     </section>
   )
 } 
