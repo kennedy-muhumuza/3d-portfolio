944984f3d06c2058c7e127e83eafab04797ec26c
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-08-12 21:24:50 +0300

    set types and declarations

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -2,16 +2,24 @@ import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
+import * as THREE from "three";
 
 const Stars = (props) => {
-  const ref = useRef();
+  const ref = useRef<THREE.Group>();
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+  // useFrame((state, delta) => {
+  //   ref.current.rotation.x -= delta / 10;
+  //   ref.current.rotation.y -= delta / 15;
+  // });
 
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+  
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module 'maath/random/dist/maath-random.esm' {
+    const random: any;
+    export default random;
+  }
+  
